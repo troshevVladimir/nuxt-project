4459494c55cda4f100f60f3a9ffe4f5ce6761156
troshevVladimir <troshevvladimir@gmail.com>
2021-12-21 07:43:02 +0300

    add scss variables

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,13 +16,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "~/assets/SCSS/variables.scss";
+
 .container {
   display: flex;
-  padding: 32px;
   column-gap: 16px;
+  flex-direction: column;
 
-  @media screen and(max-width: 650px) {
-    flex-direction: column;
+  @media screen and (min-width: $tablet) {
+    flex-direction: row;
   }
 }
 </style>
